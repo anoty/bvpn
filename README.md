8a5c82377d9d71cd7e8db8d22bdcc73f75950969
anjunyou <anjunyou@bilibili.com>
2017-07-05 16:10:21 +0800

    bvpn

---
+++ README.md
@@ -0,0 +1,20 @@
+##bvpn
+由于vpn的密码是由用户密码+google验证码组成的，所以每次网络不稳定的时候，都要重新输入密码，十分麻烦，所以bvpn对 openvpn 和 google 验证码封装，在vpn断线的时候自动更新google验证码，然后重新连接vpn
+###安装
+```
+go get github.com/anoty/bvpn
+```
+###配置
+```
+ovpn = "openvpn命令行程序路径"
+cfg = "openvpn配置文件路径"
+pass = "openvpn密码文件路径"
+username = "vpn账户"
+password="vpn密码"
+secretKey = "google 验证码 secret，不是手机上的那6个数字，是在第一次初始化 google 验证码的时候二维码对应的 secret"
+```
+###使用
+bvpn需要管理员权限
+```
+bvpn -conf=bvpn.toml
+```
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base32"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	Ovpn      string
+	Cfg       string
+	Pass      string
+	Username  string
+	Password  string
+	SecretKey string
+}
+
+var conf Config
+
+func toBytes(value int64) []byte {
+	var result []byte
+	mask := int64(0xFF)
+	shifts := [8]uint16{56, 48, 40, 32, 24, 16, 8, 0}
+	for _, shift := range shifts {
+		result = append(result, byte((value>>shift)&mask))
+	}
+	return result
+}
+
+func toUint32(bytes []byte) uint32 {
+	return (uint32(bytes[0]) << 24) + (uint32(bytes[1]) << 16) +
+		(uint32(bytes[2]) << 8) + uint32(bytes[3])
+}
+
+func oneTimePassword(key []byte, value []byte) uint32 {
+	hmacSha1 := hmac.New(sha1.New, key)
+	hmacSha1.Write(value)
+	hash := hmacSha1.Sum(nil)
+
+	offset := hash[len(hash)-1] & 0x0F
+
+	hashParts := hash[offset : offset+4]
+	hashParts[0] = hashParts[0] & 0x7F
+	number := toUint32(hashParts)
+
+	pwd := number % 1000000
+	return pwd
+}
+
+func googleAuthenticatorCode() (s string) {
+	inputNoSpaces := strings.Replace(conf.SecretKey, " ", "", -1)
+	inputNoSpacesUpper := strings.ToUpper(inputNoSpaces)
+	key, err := base32.StdEncoding.DecodeString(inputNoSpacesUpper)
+	if err != nil {
+		log.Fatal(err)
+	}
+	epochSeconds := time.Now().Unix()
+	pwd := oneTimePassword(key, toBytes(epochSeconds/30))
+	s = fmt.Sprintf("%06d", pwd)
+	return
+}
+
+func resetPassFile(code string) {
+	pass := conf.Pass
+	f, err := os.Create(pass)
+	defer f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.WriteString(conf.Username + "\r\n" + conf.Password + code + "\r\n")
+}
+
+func connect() {
+	argv := []string{"--config", conf.Cfg, "--connect-retry", "0", "--auth-user-pass", conf.Pass}
+	cmd := exec.Command(conf.Ovpn, argv[0:]...)
+	o, _ := cmd.Output()
+	fmt.Println(string(o))
+	cmd.Start()
+	cmd.Wait()
+}
+
+func main() {
+	var confPath string
+	flag.StringVar(&confPath, "conf", "", "config path")
+	flag.Parse()
+	if confPath == "" {
+		log.Fatal("conf err")
+	}
+	if _, err := toml.DecodeFile(confPath, &conf); err != nil {
+		log.Fatal(err)
+	}
+	for {
+		resetPassFile(googleAuthenticatorCode())
+		connect()
+	}
+}
